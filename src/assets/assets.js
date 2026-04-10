c93967e6bfe6937d017af3c24ec9d325a79e62c0
unknown <manglibopaul@gmail.com>
2026-04-10 16:24:25 +0800

    fix: remove all references to deleted assets from assets.js (build error)

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -6,56 +6,34 @@ const sunflower_model = '/models/sunflower.usdz';
 
 
 
-import logo from './logo.png'
-import add_icon from './add_icon.png'
-import order_icon from './order_icon.png'
-import upload_area from './upload_area.png'
-import parcel_icon from './parcel_icon.svg'
-import bin_icon from './bin_icon.png'
-import dropdown_icon from './dropdown_icon.png'
-import cart_icon from './cart_icon.png'
-import cross_icon from './cross_icon.png'
-import exchange_icon from './exchange_icon.png'
-import hero_img from './hero_img.png'
-import menu_icon from './menu_icon.png'
-import paypal_logo from './paypal_logo.png'
-import profile_icon from './profile_icon.png'
-import quality_icon from './quality_icon.png'
-import razorpay_logo from './razorpay_logo.png'
-import search_icon from './search_icon.png'
-import star_dull_icon from './star_dull_icon.png'
-import star_icon from './star_icon.png'
-import stripe_logo from './stripe_logo.png'
-import support_img from './support_img.png'
-import visa_logo from './visa_logo.png'
-import qr_code from './qr_code.png'
-import flowers from './flowerss.png'
-import keychains from './keychain.png'
-import accessories from './accessories.png'
-import coasters from './coasters.png'
+import logo from './logo.png';
+import add_icon from './add_icon.png';
+import order_icon from './order_icon.png';
+import upload_area from './upload_area.png';
+import parcel_icon from './parcel_icon.svg';
+import bin_icon from './bin_icon.png';
+import dropdown_icon from './dropdown_icon.png';
+import cart_icon from './cart_icon.png';
+import cross_icon from './cross_icon.png';
+import exchange_icon from './exchange_icon.png';
+import hero_img from './hero_img.png';
+import menu_icon from './menu_icon.png';
+import paypal_logo from './paypal_logo.png';
+import profile_icon from './profile_icon.png';
+import quality_icon from './quality_icon.png';
+import razorpay_logo from './razorpay_logo.png';
+import search_icon from './search_icon.png';
+import star_dull_icon from './star_dull_icon.png';
+import star_icon from './star_icon.png';
+import stripe_logo from './stripe_logo.png';
+import support_img from './support_img.png';
+import visa_logo from './visa_logo.png';
+import qr_code from './qr_code.png';
+
+// Removed deleted asset imports
 
 export const assets = {
     logo,
-    p_img7,
-    p_img7_2,
-    p_img7_3,
-    p_img7_4,
-    p_img7_5,
-    p_img8,
-    p_img8_2,
-    p_img11,
-    p_img12,
-    p_img14,
-    p_img15,
-    p_img16,
-    p_img17,
-    tulip_model,
-    sunflower_model,
-    flowers,
-    keychains,
-    accessories,
-    coasters,
-    qr_code,
     add_icon,
     order_icon,
     upload_area,
@@ -75,9 +53,12 @@ export const assets = {
     search_icon,
     star_dull_icon,
     star_icon,
-            // modelUrl: catpaw_model,
     support_img,
     visa_logo,
-}
+    qr_code,
+    // 3D model paths
+    tulip_model,
+    sunflower_model,
+};
 export const products = [];
 
