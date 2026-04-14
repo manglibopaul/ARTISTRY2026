d3b711d7893abe1e62a4f36d5f52c2e3e55bcb19
unknown <manglibopaul@gmail.com>
2026-04-14 15:55:48 +0800

    chore(ui): remove three-dot quick button from navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -246,11 +246,7 @@ const Navbar = () => {
           </Link>
         )}
 
-        <button onClick={() => setRightSidebarOpen(true)} aria-label='Open quick menu' className='w-6 h-6 flex items-center justify-center sm:hidden p-1 mr-2'>
-          <svg className='w-5 h-5 text-gray-700' fill='none' stroke='currentColor' strokeWidth='1.5' viewBox='0 0 24 24'>
-            <path strokeLinecap='round' strokeLinejoin='round' d='M12 6h.01M12 12h.01M12 18h.01' />
-          </svg>
-        </button>
+        
         <img onClick={()=>setVisible(true)} src={assets.menu_icon} className='w-6 cursor-pointer sm:hidden p-1' alt="" loading="lazy" />
       </div>
 
