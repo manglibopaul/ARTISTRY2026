b97846ef89a796fb76d5b7e46d184bdfe0d83fbb
unknown <manglibopaul@gmail.com>
2026-04-26 19:41:18 +0800

    feat(admin): add mobile admin support page and route (/admin/support)

---
+++ src/pages/AdminSupport.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import AdminSupportChat from '../components/AdminSupportChat'
+
+const AdminSupport = () => {
+  return (
+    <div className='py-4 sm:py-6'>
+      <div className='max-w-3xl mx-auto'>
+        <AdminSupportChat />
+      </div>
+    </div>
+  )
+}
+
+export default AdminSupport
