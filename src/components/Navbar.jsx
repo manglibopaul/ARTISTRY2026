e1190c5a409cd1437d5cdb1df0bbbbb59108bb64
unknown <manglibopaul@gmail.com>
2026-04-29 21:28:07 +0800

    Fix: use <header> wrapper for Navbar root to ensure valid JSX

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -163,7 +163,7 @@ const Navbar = () => {
   }, [userToken])
 
   return (
-    <div>
+    <header>
     <div className='fixed inset-x-0 top-0 z-50 backdrop-blur-sm bg-white/70 border-b border-gray-100'>
       <div className='max-w-6xl mx-auto px-4 sm:px-6'>
         <div className='relative z-10 flex items-center justify-between py-3 sm:py-4 font-medium'>
@@ -278,7 +278,7 @@ const Navbar = () => {
             </div>
         </div>
 
-    </div>
+    </header>
   </div>
 
     {/* Cart Slideout */}
